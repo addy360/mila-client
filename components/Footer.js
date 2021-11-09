0d7e3d26fda9bb305745d281aed71e1c2be481aa
addy360 <abbyboy360@gmail.com>
2021-11-09 17:45:17 +0300

    about page

--- components/Footer.js
+++ components/Footer.js
@@ -2,9 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="bg-yellow-900 py-9 mt-5">
+    <footer className="bg-prisec-200 py-4 mt-5">
       <div>
-        <p className="text-center">Copyright @ 2021. All rights are reserved</p>
+        <p className="text-center text-primary-900">
+          Copyright @ 2021. All rights are reserved
+        </p>
       </div>
     </footer>
   );
--- pages/about.js
+++ pages/about.js
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import React from "react";
+import Nav from "../components/Nav";
 
 function about() {
   return (
@@ -7,8 +8,27 @@ function about() {
       <Head>
         <title>About page</title>
       </Head>
-      <div>
-        <h4>About page</h4>
+      <Nav />
+      <div className="container mx-auto pt-10">
+        <h4 className="text-secondary-900 font-thin text-center text-8xl mb-5">
+          About
+          <span className="text-primary-800 font-normal ">
+            Ablo <span className="animate-ping">💥</span>
+          </span>
+        </h4>
+        <div className="md:w-3/4 p-4 mx-auto text-justify font-mono">
+          <p>
+            <span className="text-prisec-900 text-3xl font-oswald ">
+              Ablo💥
+            </span>
+            is a web client clean minimalistic 🏄 user interface for Millard Ayo
+            blog. It is built from the love 💓 of the blog of course which is
+            keeping lazy people 🙊🙊🙊 up to date with the current news👊.
+            <span className="text-prisec-900 animate-pulse ml-5">
+              🤸F.E.P 🤫
+            </span>
+          </p>
+        </div>
       </div>
     </>
   );
