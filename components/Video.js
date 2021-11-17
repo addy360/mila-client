98a5f24b0a5136511501326f4c13b3759c760fe3
addy360 <abbyboy360@gmail.com>
2021-11-17 13:57:09 +0300

    remove ;

--- components/Video.js
+++ components/Video.js
@@ -4,12 +4,12 @@ function Video({ src }) {
   return (
     <div className="w-full h-[0px] pb-[56.75%] relative">
       <iframe
+        allowFullScreen
         loading="lazy"
         src={src}
         frameBorder="0"
         className="absolute w-full h-full"
       ></iframe>
-      ;
     </div>
   );
 }
