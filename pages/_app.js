5cc1b3680f58bd127aa4a69d024311ca7d756f0b
addy360 <abbyboy360@gmail.com>
2022-01-06 17:57:55 +0300

    added metas

--- pages/_app.js
+++ pages/_app.js
@@ -28,6 +28,36 @@ function MyApp({ Component, pageProps }) {
           property="og:image"
           content="https://avatars.githubusercontent.com/u/51880591?v=4?s=400"
         />
+
+        <meta
+          name="twitter:image:src"
+          content="https://avatars.githubusercontent.com/u/51880591?v=4?s=400"
+        />
+        <meta name="twitter:site" content="@github" />
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="addy360 - Overview" />
+        <meta
+          name="twitter:description"
+          content="What for my bio? Programming I guess. addy360 has 49 repositories available. Follow their code on GitHub."
+        />
+        <meta
+          property="og:image"
+          content="https://avatars.githubusercontent.com/u/51880591?v=4?s=400"
+        />
+        <meta
+          property="og:image:alt"
+          content="What for my bio? Programming I guess. addy360 has 49 repositories available. Follow their code on GitHub."
+        />
+        <meta property="og:site_name" content="GitHub" />
+        <meta property="og:type" content="profile" />
+        <meta property="og:title" content="addy360 - Overview" />
+        <meta property="og:url" content="https://github.com/addy360" />
+        <meta
+          property="og:description"
+          content="What for my bio? Programming I guess. addy360 has 49 repositories available. Follow their code on GitHub."
+        />
+        <meta property="profile:username" content="addy360" />
+
         <link rel="preconnect" href="https://fonts.googleapis.com" />
         <link
           rel="preconnect"
